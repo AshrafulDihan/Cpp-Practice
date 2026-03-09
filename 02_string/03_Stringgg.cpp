ef3c605779ddcf1d6585f6be6c04321347d0cc36
AshrafulDihan <ashrafulalamdihan@gmail.com>
2026-03-10 03:11:46 +0600

    str

--- 02_string/03_Stringgg.cpp
+++ 02_string/03_Stringgg.cpp
@@ -1,3 +1,29 @@
 //
 // Created by USER on 3/6/2026.
 //
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string str ="Dihan";
+
+    //str.size() -- string size
+    cout<<"Size: "<<str.size()<<endl;
+
+    //string Input
+
+    string school;
+    cout<<"Enter your school name: ";
+    getline(cin , school);
+
+    cout<<"Your school is: " <<school<<endl;
+
+    cout<<str.find('n')<<endl;
+    cout<<str.insert(3,"78")<<endl;
+
+    cout<<str.erase(2,4)<<endl;
+
+    cout<<str.replace(0,3,"Ashraful")<<endl;
+}
